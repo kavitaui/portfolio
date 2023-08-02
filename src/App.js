5c12890cbfa4f470720483c57032e354866c31e5
kavitaui <122557323+kavitaui@users.noreply.github.com>
2023-08-02 13:26:05 +0000

    adding home ,resume and project componants

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import "./styles.css";
 import Header from "./Componants/SharedComponant/Header";
 import Footer from "./Componants/SharedComponant/Footer";
+import Contact from "./Componants/Contact";
+import HomeComponant from "./Componants/HomeComponant";
+import Projects from "./Componants/Projects";
+import Resume from "./Componants/Resume";
 export default function App() {
   return (
     <div className="App">
@@ -8,6 +12,10 @@ export default function App() {
     
       <Header/>
       <Footer/>
+      <HomeComponant/>
+<Projects/>
+<Resume/>
+      <Contact/>
     </div>
   );
 }
