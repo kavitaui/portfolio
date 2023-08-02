223cdb92f4c2f20f447dd0116292d5c1f68da185
kavitaui <122557323+kavitaui@users.noreply.github.com>
2023-08-02 12:58:29 +0000

    adding componants

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import "./styles.css";
-
+import Header from "./Componants/SharedComponant/Header";
+import Footer from "./Componants/SharedComponant/Footer";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      
+    
+      <Header/>
+      <Footer/>
     </div>
   );
 }
