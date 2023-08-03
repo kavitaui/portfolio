11cb9feb4068370d3e02437493a3c299e043248e
kavitaui <122557323+kavitaui@users.noreply.github.com>
2023-08-03 10:17:17 +0000

    project near complition

--- src/Componants/SharedComponant/Footer.js
+++ src/Componants/SharedComponant/Footer.js
@@ -1,7 +1,26 @@
 export default function Footer(){
     return( 
         <div className="footer">
-             <h1></h1>
+            <hr className = "new1"></hr>
+            <div className ="container">
+                <div claasName = "row">
+                    <div className = "col-3 px-2 py-2">
+                        Phone<br>123-456-7890</br>
+
+                    </div>
+                    <div className = "col-3 px-2 py-2">
+                     Email<br>info@mysite.com</br>
+                    </div>
+                    <div className = "col-3 px-2 py-2">
+                        Follow Me
+                        <br>in twitter</br>
+                    </div>
+                    <div className = "col-3 px-2 py-2">
+                        2023 By Kavita Pandey
+                      <br>  proudly created with wix.com</br>
+                    </div>
+                </div>
+            </div>
         </div>
    
     );
--- src/Componants/SharedComponant/Header.js
+++ src/Componants/SharedComponant/Header.js
@@ -1,19 +1,24 @@
 export default function Header() {
   return (
     <div className="container">
-      
-        <div className = "col-12">
-          <div className = "row">
-            <div className = "col-7 px-2 py-5" >
-              <div className = "row">
+
+      <div className="col-12">
+        <div className="row">
+          <div className="col-7 px-2 py-5" >
+            <div className="row">
               <span className="logo-wrapper"><span className="bold logo"><span className="circle"></span>Kavita Pandey</span> Editor</span>
-              </div>
-         
-        </div>
-      <div className = "col-5 px-2 py-5">
-      Resume Projects contact</div>
+            </div>
+
+          </div>
+          <div className="col-5 px-2 py-5">
+            <ul className="list-inline nav-top">
+              <li><a href="">Resume</a></li>
+              <li><a href="">Project</a></li>
+              <li><a href="">Contact</a></li>
+            </ul>
+          </div>
       </div>
-      </div>
-      </div>
+    </div>
+      </div >
   );
 }
