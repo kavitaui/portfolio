0f587c1692f92a84642b311238ee00d6a9387210
kavitaui <122557323+kavitaui@users.noreply.github.com>
2023-08-07 17:33:12 +0000

    Resume Page Design Commit

--- src/Componants/Projects.js
+++ src/Componants/Projects.js
@@ -1,3 +1,131 @@
 export default function Projects(){
-    return (<>PROJECT PAGE</>);
+    return (<div className="container px-5 py-2" >
+    <div className="col-2"></div>
+    <div className="col-10">
+        <div className="h1">Resume</div>
+    </div>
+    <div className="row">
+        <div className="col-12" >
+            <div className="row">
+
+                <div className="col-6 ">
+                    Work
+                    <div className="bold">Experience</div>
+                </div>
+                <div className="col-6 px-2 -y-2">
+                    <div className="row">
+                        <div className="col-3 px-2 py-2 ">
+                            2035 - Present
+                        </div>
+
+                        <div className="col-7 px-2 py-2 ">
+                            <span classname="bold">Editor </span>
+                            <div className="font">
+                                I'm a paragraph. Click here to add your own
+                                text and edit me. It’s easy. Just click “Edit Text”
+                                or double click me to add your own content
+                                and make changes to the font.</div>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-3 px-2 py-2 ">
+                            2035 - 2035
+                        </div>
+                        <div className="col-7 px-2 py-2 ">
+                            <span classname="bold">Writer at Large </span>
+                            <div className="font">I'm a paragraph. Click here to add your own
+                                text and edit me. It’s easy. Just click “Edit Text”
+                                or double click me to add your own content
+                                and make changes to the font.</div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+    <hr className="new1"></hr>
+    <div className="row">
+        <div className="col-12" >
+            <div className="row">
+
+                <div className="col-6 ">
+                    
+                    <div className="bold">Education</div>
+                </div>
+                <div className="col-6 px-2 -y-2">
+                    <div className="row">
+                        <div className="col-3 px-2 py-2 ">
+                            2035 - Present
+                        </div>
+
+                        <div className="col-7 px-2 py-2 ">
+                            <span classname="bold">Editor </span>
+                            <div className="font">
+                                I'm a paragraph. Click here to add your own
+                                text and edit me. It’s easy. Just click “Edit Text”
+                                or double click me to add your own content
+                                and make changes to the font.</div>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-3 px-2 py-2 ">
+                            2035 - 2035
+                        </div>
+                        <div className="col-7 px-2 py-2 ">
+                            <span classname="bold">Writer at Large </span>
+                            <div className="font">I'm a paragraph. Click here to add your own
+                                text and edit me. It’s easy. Just click “Edit Text”
+                                or double click me to add your own content
+                                and make changes to the font.</div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+    <hr className="new1" ></hr>
+    <div className="row">
+
+        <div className="col-6 ">
+
+
+
+            <div className="bold"> Skills
+                & Expertise</div>
+        </div>
+        <div className="col-6 px-2 -y-2">
+            <div className="row">
+                <div className="col-3 px-2 py-2 ">
+                    2035 - Present
+                </div>
+
+                <div className="col-7 px-2 py-2 ">
+                    <span classname="bold">Editor </span>
+                    <div className="font">
+                        I'm a paragraph. Click here to add your own
+                        text and edit me. It’s easy. Just click “Edit Text”
+                        or double click me to add your own content
+                        and make changes to the font.</div>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-3 px-2 py-2 ">
+                    2035 - 2035
+                </div>
+                <div className="col-7 px-2 py-2 ">
+                    <span classname="bold">Writer at Large </span>
+                    <div className="font">I'm a paragraph. Click here to add your own
+                        text and edit me. It’s easy. Just click “Edit Text”
+                        or double click me to add your own content
+                        and make changes to the font.</div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+
+</div>);
 }
