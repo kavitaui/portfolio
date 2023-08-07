1c88b3f2a8913549632651a38dd2234b5b8c051a
kavitaui <122557323+kavitaui@users.noreply.github.com>
2023-08-07 20:50:28 +0000

    make contacts

--- src/Componants/Contact.js
+++ src/Componants/Contact.js
@@ -1,3 +1,21 @@
-export default function contact(){
-    return(<>CONTACT PAGE</>);
+export default function contact() {
+    return (
+        <div className="container px-2 py-2">
+
+            <div className="col-12 px-2 py-2">
+            <div className="row"><div className="h1">Contact</div>
+            </div>
+            <div className="row px-2 py-2">
+            <div className="col-6 px-2 py-2">
+            Looking forward to hearing from you</div>
+            <div className="col-6 px-2 py-2">
+                <form action = "">
+                   <div> <label for = "fname">First Name</label></div>
+                   <div> <input type ="text" id ="fname"/></div>
+                </form>
+            </div>
+</div>
+        </div>
+        </div>
+    );
 }
