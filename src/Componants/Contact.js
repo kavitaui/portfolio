da57bf94f7e40c2da6992ea422efaed513a8f7e3
kavitaui <122557323+kavitaui@users.noreply.github.com>
2023-08-07 20:55:33 +0000

    form

--- src/Componants/Contact.js
+++ src/Componants/Contact.js
@@ -3,19 +3,24 @@ export default function contact() {
         <div className="container px-2 py-2">
 
             <div className="col-12 px-2 py-2">
-            <div className="row"><div className="h1">Contact</div>
+                <div className="row"><div className="h1">Contact</div>
+                </div>
+                <div className="row px-2 py-2">
+                    <div className="col-6 px-2 py-2">
+                        Looking forward to hearing from you</div>
+                    <div className="col-6 px-2 py-2">
+                        <div class="row px-2 py-2">
+                            <div class="col-6">
+                                <input type="text" class="form-control" placeholder="First name" aria-label="First name"/>
+                            </div>
+                            <div class="col-6">
+                                <input type="text" class="form-control" placeholder="Last name" aria-label="Last name"/>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
             </div>
-            <div className="row px-2 py-2">
-            <div className="col-6 px-2 py-2">
-            Looking forward to hearing from you</div>
-            <div className="col-6 px-2 py-2">
-                <form action = "">
-                   <div> <label for = "fname">First Name</label></div>
-                   <div> <input type ="text" id ="fname"/></div>
-                </form>
-            </div>
-</div>
-        </div>
         </div>
     );
 }
