ad4f6a318cd4d23af3edf869d4dd637bd57c94b7
kavitaui <122557323+kavitaui@users.noreply.github.com>
2023-08-11 13:31:35 +0100

    form submittion

--- src/Componants/Contact.js
+++ src/Componants/Contact.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 
 export default function contact() {
+    const [suceessMsg, setSucessMsg] = useState("");
 
     const [formData, setFormData] = useState({
         fname: '',
@@ -22,7 +23,18 @@ export default function contact() {
         e.preventDefault();
 
         if (validateForm()) {
+          setSucessMsg("Thank you for sharing details!!, we will get back to you soon.")
+            setFormData( {fname: '',
+            lname: '',
+            email: '',
+            subject: '',
+            message: '' })
+         setTimeout(() => {
+            setSucessMsg('')
+         }, 3000);            
 
+
+            
         }
     };
     const handleChange = (e) => {
@@ -105,7 +117,6 @@ export default function contact() {
 
 
     return (
-
         <div className="container px-2 py-2">
 
             <div className="col-12 px-2 py-2">
@@ -120,20 +131,21 @@ export default function contact() {
                             info@mysite.com</div>
                     </div>
                   
+                    
                     <div className="col-6 px-2 py-2">
                         <div className="row ">
                             <div className="col-6">
                                
                                     <label for="fname" className="form-label"><h5>First Name</h5></label>
                                     <div className="col-12">
-                                        <input type="text" onChange={handleChange} className="form-control" placeholder="first name" aria-label="first name" name="fname" />
+                                        <input type="text" onChange={handleChange} className="form-control" placeholder="first name" aria-label="first name" name="fname" value={formData.fname}/>
                                         <p>{errors.fname.length > 0 ? errors.fname : null}</p>
                                     </div>
                             </div>
                             <div className="col-6">
                                 <label for="lname" className="form-label"><h5>Last Name</h5></label>
                                 <div className="col-12">
-                                    <input type="text" onChange={handleChange} className="form-control" placeholder="Last name" aria-label="Last name" name="lname" /></div>
+                                    <input type="text" onChange={handleChange} className="form-control" placeholder="Last name" aria-label="Last name" name="lname" value={formData.lname}/></div>
                                 <p>{errors.lname.length > 0 ? errors.lname : null}</p>
                             </div>
 
@@ -142,20 +154,20 @@ export default function contact() {
                                 <label for="Email" className="form-label"><h5>Email*</h5></label>
 
                                 <div className="col-12">
-                                    <input type="text" onChange={handleChange} className="form-control" placeholder="email" name="email" aria-label="first name" />
+                                    <input type="text" onChange={handleChange} className="form-control" placeholder="email" name="email" aria-label="first name" value={formData.email}/>
                                     <p>{errors.email.length > 0 ? errors.email : null}</p>
                                 </div>
                             </div>
                             <div className="col-6">
                                 <label for="subject" className="form-label"><h5>Subject</h5></label>
                                 <div className="col-12">
-                                    <input type="text" onChange={handleChange} className="form-control" placeholder="subject" name="subject" aria-label="subject" /></div>
+                                    <input type="text" onChange={handleChange} className="form-control" placeholder="subject" name="subject" aria-label="subject" value={formData.subject}/></div>
                                 <p>{errors.subject.length > 0 ? errors.subject : null}</p>
                             </div>
                             <div className="col-6">
                                 <label for="message" className="form-label"><h5>Message</h5></label>
                                 <div className="col-12">
-                                    <textarea className="form-control" onChange={handleChange} name="message" style={{ Height: '200px', resize: 'vertical' }}
+                                    <textarea className="form-control" onChange={handleChange} value={formData.message} name="message" style={{ Height: '200px', resize: 'vertical' }}
                                         rows="6" cols="50" id="FormControlInput1" />
                                     <p>{errors.message.length > 0 ? errors.message : null}</p>
                                 </div>
@@ -163,15 +175,15 @@ export default function contact() {
                             <div className="col-6">
                                 <div className="mt-4"> <button className="resume" onClick={handleSubmit}>Submit</button> </div>
                             </div>
-
+                                <p>{suceessMsg.length?suceessMsg:null}</p>
                         </div>
 
                     </div>
 
                 </div>
-               
+               </div>
             </div>
-        </div>
+        
 
 
 
