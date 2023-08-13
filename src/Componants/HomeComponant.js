e50a9a087bfe946fa88aef5c6a03305b324afd2f
kavitaui <122557323+kavitaui@users.noreply.github.com>
2023-08-13 20:44:58 +0100

    complete home componant

--- src/Componants/HomeComponant.js
+++ src/Componants/HomeComponant.js
@@ -2,6 +2,7 @@ import image from "../Asset/image/nicolRider.jpg"
 import { motion } from "framer-motion";
 import { Link } from "react-router-dom";
 import content from "../Asset/content.json";
+import { useEffect } from "react";
 export default function HomeComponant(props) {
   const routeVariants = {
     initial: {
@@ -30,6 +31,11 @@ export default function HomeComponant(props) {
       },
     },
   };
+
+  useEffect(()=>{
+    console.log("Hello")
+    //axios call to get json
+  }, [])
   return (
     <motion.div
       variants={routeVariants}
@@ -45,12 +51,12 @@ export default function HomeComponant(props) {
 
               <div className="row" >
                 <div className="col-6 px-2 py-2">
-                 <motion.span  variants={childVariants} initial="initial" animate="final"><img className="pic" src={image} /></motion.span>
+                 <motion.span  variants={childVariants} initial="initial" animate="final"><img className="pic" src={ content.home.image.path} /></motion.span>
                 </div>
                 <div className="col-6 px-2 py-2 pt-5"><motion.span  variants={childVariants} initial="initial" animate="final" className="h1">{content.home.title}</motion.span>
-                  <motion.div   variants={childVariants} initial="initial" animate="final"><h5>{content.home.subtitle} </h5> </motion.div>
-                  <motion.div  variants={childVariants} initial="initial" animate="final"> <div className="font">I'm a paragraph. Click here to add your own text and edit me.I'm a great place for
-                    you to tell a story and let your users to know a little more about you.</div></motion.div>
+                  <motion.div   variants={childVariants} initial="initial" animate="final"><h5 className="mt-4">{content.home.subtitle} </h5> </motion.div>
+                  <motion.div  variants={childVariants} initial="initial" animate="final"> <div className="font  my-4">{content.home.description}
+                  </div></motion.div>
                    {content.home.buttons.map(item =>(
                     < Link to = {item.route}> <motion.button  initial={item.animate.initial} animate={item.animate.final} transition={item.animate.transition} className={item.class+ ' mt-3'} > {item.name}</motion.button></Link>
                    ))}
